0f4e221d60586e35f2501ff4ebb33c22bd86dc43
yarvoy <134600591+yarvoy@users.noreply.github.com>
2023-05-25 13:00:41 +0300

    Add files via upload

---
+++ lab5/lab5/src/main/java/com/ontu/lab5/controller/HelloWorldController.java
@@ -0,0 +1,10 @@
+package com.ontu.lab5.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController {
+@GetMapping("/hello-world")
+public String HelloWorld(){
+return "Hello World";
+} 
+}
